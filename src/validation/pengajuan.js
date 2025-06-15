3830c2caeb40c070b66c300189fa8dbf2fb09bd8
ashly2-pixel <ashly2@gmaels.com>
2025-06-15 08:24:50 +0800

    Update dokumentasi kode

--- src/validation/pengajuan.js
+++ src/validation/pengajuan.js
@@ -4,5 +4,6 @@ const createPengajuan = Joi.object({
   user_id: Joi.number().required(),
   posisi: Joi.string().required(),
 });
+// Perubahan minor untuk konsistensi kode 
 
 export { createPengajuan };
